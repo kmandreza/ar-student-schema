2421cfddd0069fd04420073c6a3e0d673161ce27
Apprentice <apprentice@dbc11.local>
2013-02-14 14:01:07 -0800

    migrations, first obj model, yet to do...validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,17 @@
 require_relative '../../db/config'
 
+
 # implement your Student model here
+class Student < ActiveRecord::Base
+
+  def name
+    self.first_name + ' ' + self.last_name
+  end
+
+  def age
+
+    now = Date.today
+    age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  
+  end
+end
