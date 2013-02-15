d1dfc13895c5909b52e6c7cd87fd2a1f8d915dfd
Apprentice <apprentice@dbc11.local>
2013-02-14 16:07:56 -0800

    FINISHEDgit add .git add .git add . LOOK AT MY CODEgit add .git add .

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,16 +2,21 @@ require_relative '../../db/config'
 
 
 # implement your Student model here
+
 class Student < ActiveRecord::Base
+  attr_reader :age
+
+validates :email, :presence => true, :uniqueness => true
+validates_format_of :email, :with => /(\w+)@(\w+).(\w{2,})/
+validates_numericality_of :age, :greater_than => 5.00
+validates_format_of :phone, :with => /\d{3}.*\d{3}.*\d{4}/
 
   def name
     self.first_name + ' ' + self.last_name
   end
 
   def age
-
     now = Date.today
     age = now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
-  
   end
 end
